724476caaabfc622dedf4f6ece14e6d68c06dafb
Francisco Santos <3703930+thaenor@users.noreply.github.com>
2020-12-14 00:36:11 +0000

    Add Interface components and api call

--- src/components/SideForm/index.tsx
+++ src/components/SideForm/index.tsx
@@ -1,24 +1,81 @@
-import * as React from "react";
+import React, { useState } from "react";
 import GreetingMsg from "../GreetingMsg";
+import { postData } from "../../logic/client";
+import Select from "react-select";
+import DatePicker from "react-datepicker";
 import { SideFormSection, Form, Label, Input, SubmitBtn } from "./styles";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function SideForm() {
+  const [firstName, setFirstName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [countries, setCountries] = useState("");
+  const now = new Date();
+  const [bday, setBday] = useState(now);
+
+  const options = [
+    { value: "Portugal", label: "Portugal" },
+    { value: "France", label: "France" },
+    { value: "England", label: "England" },
+    { value: "Mexico", label: "Mexico" },
+    { value: "USA", label: "USA" },
+    { value: "Japan", label: "Japan" },
+    { value: "Turkey", label: "Turkey" },
+    { value: "Lebanon", label: "Lebanon" }
+  ];
+
+  const handleNameInput = (evt) => setFirstName(evt.target.value);
+
+  const handleSurnameInput = (evt) => setSurname(evt.target.value);
+
+  const handleSelect = (evt) => setCountries(evt.value);
+
+  const handleDate = (date) => setBday(date);
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const data = {
+      first_name: firstName,
+      last_name: surname,
+      country: countries,
+      birth_day: bday
+    };
+
+    postData("https://example.com/answer", data).then((result) => {
+      if (result.ok) {
+        console.log(result.json());
+      }
+    });
+  };
+
   return (
     <SideFormSection>
       <Form>
         <Label htmlFor="nameInput">Name:</Label>
-        <Input type="text" name="name" id="nameInput" />
+        <Input
+          type="text"
+          name="name"
+          id="nameInput"
+          onChange={handleNameInput}
+        />
 
         <Label htmlFor="surnameInput">Surname</Label>
-        <Input type="text" name="surname" id="surnameInput" />
+        <Input
+          type="text"
+          name="surname"
+          id="surnameInput"
+          onChange={handleSurnameInput}
+        />
 
         <Label htmlFor="countriesSelect">Countries</Label>
-        <select name="countries" id="countriesSelect"></select>
+        <Select options={options} onChange={handleSelect} />
 
         <Label htmlFor="bdayInput">Birthday:</Label>
-        <Input type="datetime" name="bday" id="bdayInput" />
+        <DatePicker selected={bday} onChange={handleDate} />
 
-        <SubmitBtn type="submit">Save</SubmitBtn>
+        <SubmitBtn type="submit" onClick={handleSubmit}>
+          Save
+        </SubmitBtn>
       </Form>
       <GreetingMsg />
     </SideFormSection>
