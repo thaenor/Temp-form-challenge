f1bca279033536e052c0c97f4eb6c81a832df929
Francisco Santos <3703930+thaenor@users.noreply.github.com>
2020-12-17 00:01:16 +0000

    fix greeting date logic

--- src/components/GreetingMsg/index.tsx
+++ src/components/GreetingMsg/index.tsx
@@ -4,11 +4,24 @@ import { GrettingsSection } from "./styles";
 export default function GreetingMsg(props) {
   const { name, country, date } = { ...props };
 
+  const getDay = (date: string) => {
+    return Number(date.split("/")[0]);
+  };
+
+  const getMonth = (date: string) => {
+    return Number(date.split("/")[1]);
+  };
+
+  const calculateDate = (date: string) => {
+    const year = Number(date.split("/")[2]);
+    const current_year = new Date().getFullYear();
+    return current_year - year;
+  };
+
   return (
     <GrettingsSection>
-      <p>
-        Hello {name} from {country} on {date}{" "}
-      </p>
+      Hello {name} from {country} on {getDay(date)} of {getMonth(date)} you will
+      be {calculateDate(date)} years old.
     </GrettingsSection>
   );
 }
